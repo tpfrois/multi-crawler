0d43d36cd9c2dde929e3ea0e041f06f4c6e8c6df
Thiago Frois <zthiagopf@gmail.com>
2021-11-07 14:04:30 -0300

    fix port bug

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
   res.send("Multi Crawler");
 });
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("Server is running on port 4000");
 });
